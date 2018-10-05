006ecde10eb294c9f8cdbe439ff07df9c029a772
Sunita-Bhende <43868777+Sunita-Bhende@users.noreply.github.com>
2018-10-04 22:53:06 -0400

    Details of Sub_query lang

---
+++ SQL_Sub_Queries2.sql
@@ -0,0 +1,12 @@
+DDL Commands:-Create table
+create table Emp (EMpID int,Ename char(10),salary money,Age int);
+
+AlterTable:-Alter add
+            Alter DataSIze
+			ALter Drop
+			Sp_Help to rename/columnname and table name
+
+Alter table Emp add ELastName char;
+Alter table Emp alter column ELastName varchar(10)
+Alter table Emp drop column EmpLastname
+
